8068771726c6c4482de4e423d4749d803d803ae9
rr- <mkurczew@gmail.com>
2015-07-13 20:41:59 +0200

    Removed unused code

--- open_ext.py
+++ open_ext.py
@@ -69,32 +69,3 @@ class open_ext:
 
     def write_u32_le(self, x):
         self.file.write(struct.pack('<I', x))
-
-    class ArgParser(object):
-        def __init__(self, mode='r', bufsize=None):
-            self._mode = mode
-            self._bufsize = bufsize
-
-        def __call__(self, string):
-            # the special argument "-" means sys.std{in,out}
-            if string == '-':
-                if 'r' in self._mode:
-                    return _sys.stdin
-                elif 'w' in self._mode:
-                    return _sys.stdout
-                else:
-                    msg = _('argument "-" with mode %r' % self._mode)
-                    raise ValueError(msg)
-
-            # all other arguments are used as file names
-            if self._bufsize:
-                return open_ext(string, self._mode, self._bufsize)
-            else:
-                return open_ext(string, self._mode)
-
-        def __repr__(self):
-            args = [self._mode, self._bufsize]
-            args_str = ', '.join([repr(arg) for arg in args if arg is not None])
-            return '%s(%s)' % (type(self).__name__, args_str)
-
-
